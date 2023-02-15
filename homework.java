07cae026736e484414c152f15918f41895a78f93
Aleksandr <kossander@bk.ru>
2023-02-15 14:29:38 +0300

    Revision of the program

--- homework.java
+++ homework.java
@@ -23,15 +23,16 @@
 // Создать метод записи в файл
 // Создать метод управления работой программы
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.Scanner;
+import java.io.File; // импортируем класс для работы с файлами
+import java.io.FileNotFoundException; // импортируем класс обработки ошибки отсутствия файла
+import java.util.Scanner; // импортируем класс для чтения информации из консоли, файла и т.п.
+import java.io.PrintWriter; // Импортируем класс записи в файл
 
 public class homework {
     public static void main(String[] args) throws FileNotFoundException { 
-        int a = readFile()[0];
-        int b = readFile()[1];
+        int[] array = readFile();
+        int a = array[0];
+        int b = array[1];
         if(a == 0 && b == 0) writeFile("Не определено"); // записать в файл "Не определено"
         else if(b < 0) writeFile(negativPow(a, b)); // Вычисляем и записываем в случае отрицательной степени
         else writeFile(positivPow(a, b)); // Вычисляем и записываем в случае положительной степени
@@ -44,12 +45,16 @@ public class homework {
         String line = scanner.nextLine(); // присвоить строковой переменной следующую строку
         int[] intArray = new int[2];
         String[] stringArray = line.split(" "); // Создали массив из строки разбив по пробелам
-        int a = Integer.parseInt(stringArray[1]); // Преобразуем в число второй элемент стокового массива
-        intArray[0] = a;
-        line = scanner.nextLine(); // присвоить строковой переменной следующую строку
-        stringArray = line.split(" "); // Создали массив из строки разбив по пробелам
-        a = Integer.parseInt(stringArray[1]); // Преобразуем в число второй элемент стокового массива
-        intArray[1] = a;
+        if(stringArray[0] == "a"){
+            intArray[0] = Integer.parseInt(stringArray[1]); // Преобразуем в число второй элемент строкового массива и ставим его в числовой массив
+            stringArray = scanner.nextLine().split(" "); // Создали массив из прочтенной следующей строки разбив по пробелам минуя переменную line
+            intArray[1] = Integer.parseInt(stringArray[1]); // Преобразуем в число второй элемент строкового массива и ставим его в числовой массив
+        }
+        else {
+            intArray[1] = Integer.parseInt(stringArray[1]);
+            stringArray = scanner.nextLine().split(" ");
+            intArray[0] = Integer.parseInt(stringArray[1]);
+        }
         // scanner.hasNextLine() - проверяет, есть ли несчитанные строки
         scanner.close(); // Закрыли сканер
         return intArray;
